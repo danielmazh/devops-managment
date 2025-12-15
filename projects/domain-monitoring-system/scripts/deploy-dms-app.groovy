e0bc04917eea23ee666ad828240e0f5764f49e9a
Daniel Mazhbits <daniel.mazhbits@gmail.com>
2025-12-15 21:10:58 +0200

    Update deploy-dms-app.groovy script with detailed manual Terraform execution instructions, including initialization, planning, and applying configurations for a specific customer setup.

--- projects/domain-monitoring-system/scripts/deploy-dms-app.groovy
+++ projects/domain-monitoring-system/scripts/deploy-dms-app.groovy
@@ -2,3 +2,20 @@
 //   -var="customer_name=client-x" \
 //   -var="backend_instance_count=2" \
 //   -var="frontend_instance_count=2"
+
+// to run manually tf:
+//     cd projects/domain-monitoring-system/terraform
+//     make sure ~/.aws/credentials
+
+//     terraform init
+
+//     terraform plan \
+//     -var="customer_name=daniel-test" \
+//     -var="backend_instance_count=1" \
+//     -var="frontend_instance_count=2"
+
+//     terraform apply \
+//       -var="customer_name=daniel-test" \
+//       -var="backend_instance_count=1" \
+//       -var="frontend_instance_count=2" \
+//       -auto-approve
